907cc52760bdb5f54879c7f53736b78914b13735
Pavel Ivanov <ppivanov98@gmail.com>
2020-11-23 19:17:33 +0000

    CConsuming TheCocktailDB to display a list of cocktails and a simple filter

---
+++ BookStore_Client/JokeResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cocktail_Catalog
+{
+    public class ChuckNorrisJokeResponse
+    {
+        public string Type { get; set; }
+
+        public TheJoke Value { get; set; }
+    }
+
+    public class TheJoke
+    {
+        public string Joke { get; set; }
+    }
+}
--- BookStore_Client/Program.cs
+++ BookStore_Client/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace BookStore_Client
+namespace Cocktail_Catalog
 {
     public class Program
     {
