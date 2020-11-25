f782b4039e36cb89cdf75733a6b3279a0ccb86d5
Pavel Ivanov <ppivanov98@gmail.com>
2020-11-25 19:36:05 +0000

    Cocktail details page and search when 'Enter' is pressed

--- Cocktail_Catalog/CocktailDbResponse.cs
+++ Cocktail_Catalog/CocktailDbResponse.cs
@@ -1,25 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Cocktail_Catalog
 {
     public class CocktailDbResponse
     {
-        public CocktailDrinks[] Drinks { get; set; }
+        public CocktailDrink[] Drinks { get; set; }
     }
 
-    public class CocktailDrinks
+    public class CocktailDrink
     {
+        public string IdDrink { get; set; }
         public string StrDrink { get; set; }
         public string StrAlchoholic { get; set; }
         public string StrGlass { get; set; }
         public string StrInstructions { get; set; }
         public string StrDrinkThumb { get; set; }
         public string StrIngredient1 { get; set; }
-        public string StrIngredient2{ get; set; }
-        public string StrIngredient3{ get; set; }
+        public string StrIngredient2 { get; set; }
+        public string StrIngredient3 { get; set; }
         public string StrIngredient4 { get; set; }
         public string StrIngredient5 { get; set; }
         public string StrIngredient6 { get; set; }
@@ -48,5 +46,94 @@ namespace Cocktail_Catalog
         public string StrMeasure13 { get; set; }
         public string StrMeasure14 { get; set; }
         public string StrMeasure15 { get; set; }
+
+        public List<IngredientAndMeasure> Ingredients
+        {
+            get
+            {
+                List<IngredientAndMeasure> ingredients = GetIngredientsAndMeasuresAsList();
+
+                return ingredients;
+            }
+        }
+
+        private List<IngredientAndMeasure> GetIngredientsAndMeasuresAsList()
+        {
+            List<IngredientAndMeasure> ingredients = new List<IngredientAndMeasure>();
+            if (!string.IsNullOrEmpty(StrIngredient1))
+                AddIngredientAndMeasure(ingredients, StrIngredient1, StrMeasure1);
+            else return ingredients;
+
+            if (!string.IsNullOrEmpty(StrIngredient2))
+                AddIngredientAndMeasure(ingredients, StrIngredient2, StrMeasure2);
+            else return ingredients;
+
+            if (!string.IsNullOrEmpty(StrIngredient3))
+                AddIngredientAndMeasure(ingredients, StrIngredient3, StrMeasure3);
+            else return ingredients;
+
+            if (!string.IsNullOrEmpty(StrIngredient4))
+                AddIngredientAndMeasure(ingredients, StrIngredient4, StrMeasure4);
+            else return ingredients;
+
+            if (!string.IsNullOrEmpty(StrIngredient5))
+                AddIngredientAndMeasure(ingredients, StrIngredient5, StrMeasure5);
+            else return ingredients;
+
+            if (!string.IsNullOrEmpty(StrIngredient6))
+                AddIngredientAndMeasure(ingredients, StrIngredient6, StrMeasure6);
+            else return ingredients;
+
+            if (!string.IsNullOrEmpty(StrIngredient7))
+                AddIngredientAndMeasure(ingredients, StrIngredient7, StrMeasure7);
+            else return ingredients;
+            
+            if (!string.IsNullOrEmpty(StrIngredient8))
+                AddIngredientAndMeasure(ingredients, StrIngredient8, StrMeasure8);
+            else return ingredients;
+
+            if (!string.IsNullOrEmpty(StrIngredient9))
+                AddIngredientAndMeasure(ingredients, StrIngredient9, StrMeasure9);
+            else return ingredients;
+
+            if (!string.IsNullOrEmpty(StrIngredient10))
+                AddIngredientAndMeasure(ingredients, StrIngredient10, StrMeasure10);
+            else return ingredients;
+
+            if (!string.IsNullOrEmpty(StrIngredient11))
+                AddIngredientAndMeasure(ingredients, StrIngredient11, StrMeasure11);
+            else return ingredients;
+
+            if (!string.IsNullOrEmpty(StrIngredient12))
+                AddIngredientAndMeasure(ingredients, StrIngredient12,StrMeasure12);
+            else return ingredients;
+
+            if (!string.IsNullOrEmpty(StrIngredient13))
+                AddIngredientAndMeasure(ingredients, StrIngredient13,StrMeasure13);
+            else return ingredients;
+
+            if (!string.IsNullOrEmpty(StrIngredient14))
+                AddIngredientAndMeasure(ingredients, StrIngredient14,StrMeasure14);
+            else return ingredients;
+
+            if (!string.IsNullOrEmpty(StrIngredient15))
+                AddIngredientAndMeasure(ingredients, StrIngredient15,StrMeasure15);
+
+            return ingredients;
+        }
+        private void AddIngredientAndMeasure(List<IngredientAndMeasure> list, string ingredient, string measure)
+        {
+            list.Add(new IngredientAndMeasure
+            {
+                Ingredient = ingredient,
+                Measure = measure
+            });
+        }
+    }
+
+    public class IngredientAndMeasure
+    {
+        public string Ingredient { get; set; }
+        public string Measure { get; set; }
     }
 }
--- Cocktail_Catalog/JokeResponse.cs
+++ Cocktail_Catalog/JokeResponse.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Cocktail_Catalog
+﻿namespace Cocktail_Catalog
 {
     public class ChuckNorrisJokeResponse
     {
