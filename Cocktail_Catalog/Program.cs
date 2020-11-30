3f04a883d84e3f0e65ee70682d1b67dd80e366ea
Pavel Ivanov <ppivanov98@gmail.com>
2020-11-30 12:55:12 +0000

    Extracting the API's base urls into a config file. Adding hover effect on catalog view

--- Cocktail_Catalog/Program.cs
+++ Cocktail_Catalog/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace Cocktail_Catalog
---
+++ Cocktail_Catalog/Settings.cs
@@ -0,0 +1,13 @@
+﻿namespace Cocktail_Catalog
+{
+    public class Settings
+    {
+        public BaseURLs BaseURLs { get; set; }
+    }
+
+    public class BaseURLs
+    {
+        public string CocktailDb { get; set; }
+        public string JokeDb { get; set; }
+    }
+}
